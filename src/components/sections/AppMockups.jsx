d8ae2406718b346230843506c153356983fae3ee
aravM23 <aravmathur23@gmail.com>
2026-01-14 00:40:21 -0500

    Fix Generate button - remove Recent section, button fully visible

--- src/components/sections/AppMockups.jsx
+++ src/components/sections/AppMockups.jsx
@@ -414,24 +414,6 @@ function GenerateScreen({ goToScreen }) {
         </div>
       </div>
       
-      {/* Recent */}
-      <div className="gen-recent">
-        <p className="gen-section-label">Recent</p>
-        {recentPlaces.map((place, i) => (
-          <button key={i} className="gen-recent-item">
-            <div className="gen-recent-icon">
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <circle cx="12" cy="12" r="10"/><path d="M12 6v6l4 2"/>
-              </svg>
-            </div>
-            <div className="gen-recent-info">
-              <span className="gen-recent-name">{place.name}</span>
-              <span className="gen-recent-time">{place.time}</span>
-            </div>
-          </button>
-        ))}
-      </div>
-      
       {/* Generate CTA */}
       <button className="gen-cta">
         Generate Outfit
